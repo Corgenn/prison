04f57d965b374fb05f8ecd6c3895c6b30ee83470
Kirill Butyrin <butyrinbusiness@gmail.com>
2023-05-10 20:34:36 +0300

    creste sort by age function

--- prison.py
+++ prison.py
@@ -1,22 +1,27 @@
-prisoner = [
+prisoners = [
     {"name" : "Boris", "Age" : 19, "level_d" : 2, "freed" : False},
     {"name" : "Kiril", "Age" : 29, "level_d" : 3, "freed" : False},
     {"name" : "Oleg", "Age" : 54, "level_d" : 4, "freed" : True},
     {"name" : "Nasta", "Age" : 72, "level_d" : 1, "freed" : False},
     {"name" : "Sera", "Age" : 34, "level_d" : 3, "freed" : True},
 ];
-
 try:
-    num_function = int(input("print a 1 to sorted for age, 2 to sorted for name, 3 to sorted for level_danger"))
+    num_function = int(input("print a 1 to sorted by age, 2 to sorted by name, 3 to sorted by level_danger"));
     if 3 > num_function < 0:
         raise ValueError()
-except ValueError:
+except ValueError: 
     print ("You dont print a 1, 2 or 3!");
 
+def sort_by_age (prisoners):
+    def get_Age (prisoner):
+        return (prisoner["Age"])
+    prisoners.sort (key=get_Age)  
+    print (prisoners)
+
 match num_function:
-    case "1":
-            print (prisoner)
-    case "2":
-            print ("asd")
-    case "3":
+    case 1:
+            sort_by_age (prisoners)
+    case 2:
+            print ()
+    case 3:
             print ()
