0f6338d8fcc78cc2bd51f90f06dc821c968c9335
Kirill Butyrin <butyrinbusiness@gmail.com>
2023-05-10 18:47:49 +0300

    masiv and "match".

--- prison.py
+++ prison.py
@@ -1,7 +1,22 @@
-def testfunc(text):
-    print (text)
-testfunc("asdf")
+prisoner = [
+    {"name" : "Boris", "Age" : 19, "level_d" : 2, "freed" : False},
+    {"name" : "Kiril", "Age" : 29, "level_d" : 3, "freed" : False},
+    {"name" : "Oleg", "Age" : 54, "level_d" : 4, "freed" : True},
+    {"name" : "Nasta", "Age" : 72, "level_d" : 1, "freed" : False},
+    {"name" : "Sera", "Age" : 34, "level_d" : 3, "freed" : True},
+];
 
-a = input("Fill in function name:");
-function_name, argument = a.split();
-eval(a + "()");
+try:
+    num_function = int(input("print a 1 to sorted for age, 2 to sorted for name, 3 to sorted for level_danger"))
+    if 3 > num_function < 0:
+        raise ValueError()
+except ValueError:
+    print ("You dont print a 1, 2 or 3!");
+
+match num_function:
+    case "1":
+            print (prisoner)
+    case "2":
+            print ("asd")
+    case "3":
+            print ()
