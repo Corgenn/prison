3a051fd3b7c63d00831e55f46f1b6632849e4551
Corgenn <danilkabyturin@gmail.com>
2023-05-16 20:29:42 +0300

    complit match

--- prison.py
+++ prison.py
@@ -6,7 +6,7 @@ prisoners = [
     {"name" : "Sera", "Age" : 34, "level_d" : 3, "freed" : True},
 ];
 try:
-    num_function = int(input("print a 1 to sorted by age, 2 to sorted by name, 3 to sorted by level_danger"));
+    num_function = int(input("print a 1 to sorted by age, 2 to sorted by freed, 3 to sorted by level_danger"));
     if 3 > num_function < 0:
         raise ValueError()
 except ValueError: 
@@ -15,13 +15,28 @@ except ValueError:
 def sort_by_age (prisoners):
     def get_Age (prisoner):
         return (prisoner["Age"])
-    prisoners.sort (key=get_Age)  
-    print (prisoners)
+    prisoners.sort (key=get_Age)
+    for p in prisoners:
+        print (p);
+
+def sort_by_freed (prisoners):
+    for p in prisoners:
+        if p["freed"] == False:
+            print (p["name"], "Not escaped");
+        else:
+            print (p["name"], "Escaped");
+
+def sort_by_danger (prisoners):
+    def get_danger (prisoner):
+        return (prisoner["level_d"])
+    prisoners.sort (key=get_danger)
+    for p in prisoners:
+        print (p);
 
 match num_function:
     case 1:
-            sort_by_age (prisoners)
+            sort_by_age (prisoners);
     case 2:
-            print ()
+            sort_by_freed (prisoners);
     case 3:
-            print ()
+            sort_by_danger (prisoners);
